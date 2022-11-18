b6092b8986f5a1e05793aee21cec1409e242e8c7
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-18 11:33:25 -0300

    Add files via upload

---
+++ caesar cipher.py
@@ -0,0 +1,12 @@
+text = input("Enter your message: ")
+cipher = ''
+for char in text:
+    if not char.isalpha():
+        continue
+    char = char.upper()
+    code = ord(char) + 1
+    if code > ord('Z'):
+        code = ord('A')
+    cipher += chr(code)
+
+print(cipher)
