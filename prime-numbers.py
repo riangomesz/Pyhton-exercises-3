778a4deabf9d823ab7daf516ddaf5d02ce10226d
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-24 13:04:43 -0300

    Add files via upload

---
+++ prime-numbers.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number: "))
+print("The prime numbers found are")
+for num in range (a):
+    div = 0
+    for x in range(1,num+1):
+        resto = num % x
+        if resto == 0:
+            div += 1
+    if div == 2:
+        print(num)
