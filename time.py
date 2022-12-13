09f42364c54b1475d11d71f3ca633231f9d59a22
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-12-13 10:58:13 -0300

    Add files via upload

---
+++ time.py
@@ -0,0 +1,12 @@
+from datetime import time
+
+def trabalhando_com_time():
+    horario = time(hour=15, minute=18, second= 30)
+    print(horario)
+    horario_str = horario.strftime('%H:%M:%S')
+    print(type(horario_str))
+
+if __name__ == '__main__':
+    #trabalhando com time()
+    trabalhando_com_time()
+    
