00ad8e87e06810743a404ddbfe3124d031186f37
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-12-15 12:12:16 -0300

    Create simples.py

---
+++ simples.py
@@ -0,0 +1,11 @@
+# Simulador simples de compras em Python
+
+lista_de_compras = []
+print("Olá você está no sacolão do Verdemar.\n E acabou de ganhar um voucher com \n descontos para escolher dez alimetos.")
+
+for x in range(4):
+    alimentos = str(input(" Selecione seus alimentos: "))
+    lista_de_compras.append(alimentos)
+print("Alimentos selecionados: "+str(lista_de_compras))
+
+    
